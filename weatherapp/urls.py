8f13fed9f27f5d07b025bc5e2f35931ada35bfc0
Manoj Chandra <163059020+imanojchandra@users.noreply.github.com>
2025-04-08 11:41:22 +0530

    Delete weatherapp/urls.py

--- weatherapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from weather import views
-
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.index, name='index'),
-]
